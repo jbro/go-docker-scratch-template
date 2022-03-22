2dfd6f5bd2db17e377d5fca35a7b766c0b61f8af
Jesper B. Rosenkilde <jbr@humppa.dk>
2022-03-22 21:48:19 +0100

    Add example

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/jbro/go-docker-scratch-template
+
+go 1.18
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://www.timeanddate.com/worldclock/timezone/utc", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.Header.Add("Accept-Language", "en")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Fatalln(err)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	wtime := doc.Find("span#ct").Text()
+	wdate := doc.Find("span#ctdat").Text()
+
+	fmt.Println(wtime)
+	fmt.Println(wdate)
+
+	t, err := time.Parse("Monday, 02 January 2006 15:04:05", wdate+" "+wtime)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(t)
+
+	dk, err := time.LoadLocation("Europe/Berlin")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(t.In(dk))
+
+	os.Remove("./db")
+	db, err := sql.Open("sqlite3", "./db")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("create table clock (time text);")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	stmt, err := tx.Prepare("insert into clock(time) values(?)")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer stmt.Close()
+
+	stmt.Exec(t)
+	stmt.Exec(t.In(dk))
+	tx.Commit()
+
+	rows, err := db.Query("select time from clock")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var t string
+		err = rows.Scan(&t)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(t)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
